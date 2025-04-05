287d0d6e0db7e41ff37ad40b0a8a027d53e4eebb
maged384 <126027164+MagedTT@users.noreply.github.com>
2025-04-05 22:18:02 +0200

    Update IProduct.cs

--- ProductAPI/Core/Interfaces/IProduct.cs
+++ ProductAPI/Core/Interfaces/IProduct.cs
@@ -1,7 +1,7 @@
 using CoreLib.Interfaces;
 using ProductAPI.Core.Models;
 
-namespace ProductAPI.Application.Interfaces;
+namespace ProductAPI.Core.Interfaces;
 
 public interface IProduct : ICrud<Product>
 {
